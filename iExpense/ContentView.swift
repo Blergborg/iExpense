0c77d436b465c9d6bd130438836a06374be23317
Blergborg <pprater92@gmail.com>
2023-09-08 15:28:54 -0700

    Notes for topics needed to actually build the app.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,20 +7,76 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @StateObject private var user = User()
+    @State private var showingSheet = false
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // Wrapping the List's VStack in a NavigationView allows us to add an edit button to VStack through a .toolbar modifier that contains the EditButton(). Serriously, that's it.
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    // onDelete modifier can only be added to a ForEach View (why we're not using the List shorthand).
+                    .onDelete(perform: removeRows)
+                }
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+            /*
+             Button("Show Sheet") {
+             showingSheet.toggle()
+             }
+             .sheet(isPresented: $showingSheet) {
+             SecondView(name: "Lil Shitter")
+             }
+             */
+            /*
+             VStack {
+             Text("Your name is \(user.firstName) \(user.lastName).")
+             
+             TextField("First name", text: $user.firstName)
+             TextField("Last name", text: $user.lastName)
+             }
+             .padding()
+             */
         }
-        .padding()
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SecondView: View {
+    // how to dismiss a View
+    @Environment(\.dismiss) var dismiss
+    var name: String
+    var body: some View {
+//        Text("Hello \(name)")
+        Button("Dismiss") {
+            dismiss()
+        }
     }
 }
