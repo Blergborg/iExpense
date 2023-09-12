0250b27171bf191009a9418eaec76b48141e7fe1
Blergborg <pprater92@gmail.com>
2023-09-12 16:33:51 -0700

    Notes about encoding/decoding data through use of Codable protocol.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,14 +7,27 @@
 
 import SwiftUI
 
+// NOTE: Swift will actually generate code for archiving and unarchiving more
+// complex data instances using the Coadable protocol.
+// However, we need to tell Swift *when* to do either of these actions
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
-    // NOTE: even simpler, UserDefaults provides this @AppStorage wrapper we can use instead of @State
-    // Great for simple use cases like this.
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
+        Button("Save User") {
+            // This is how we tell Swift to encode our codable data types.
+            // *(Decoding raw data (like JSON) into Codable types is done just by making an instance of the apropriate Decoder())
+            // **(Other formats besides JSON are available, but it is a very common choice)
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
         }
     }
 }
