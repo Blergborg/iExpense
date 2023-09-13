748ca562f250142da499a9c0bba59ee9650069f9
Blergborg <pprater92@gmail.com>
2023-09-12 18:43:28 -0700

    Updated ExpenseItem to conform to Codable protocol. Added didSet listener to items property in Expenses that encodes and saves changes to UserDefualts under the key "Items" Add custom initializer to Expenses that checks data to load from UserDefaults under the key "Items", then decodes the data (if found) into an array of ExpenseItems and sets the items property to the decoded array.

--- iExpense/models/ExpenseItem.swift
+++ iExpense/models/ExpenseItem.swift
@@ -10,8 +10,8 @@ import Foundation
 // Represents a single expense
 // Conforms to Identifiable protocol (only requirement is it has an "id" property that is a unique value)
 // ( By adding the Identifiable protocol dyanmic Lists that use ForEach don't need to set an id, Swift knows to use this one)
-struct ExpenseItem: Identifiable {
-    let id = UUID()  // ask Swift to generate a unique id for expense items.
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()  // ask Swift to generate a unique id for expense items. NOTE: update this to var from let to make Codable work.
     let name: String
     let type: String
     let amount: Double
--- iExpense/models/Expenses.swift
+++ iExpense/models/Expenses.swift
@@ -11,6 +11,40 @@ import Foundation
 // Conforms to ObservableObject protocol and marks the collection w/ @Published
 // so change annoucements get sent whenever any collection items are modified.
 
+/*
+ We’re going to leverage four important technologies to help us save and load data in a clean way:
+
+     X The Codable protocol, which will allow us to archive all the existing *expense items* ready to be stored.
+     X UserDefaults, which will let us save and load that archived data.
+     X A custom initializer for the *Expenses class*, so that when we make an instance of it we load any saved data from UserDefaults
+     X A didSet property observer on the *items property of Expenses*, so that whenever an item gets added or removed we’ll write out changes.
+
+ */
+
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        // Save changes to data.
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        // try to load data when app first needs it.
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            // if items were loaded, decode them
+            // NOTE: To avoid confusion – to say that we mean we’re referring to the type [ExpenseItem] itself,
+            // known as the type object – we write .self after it.
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                // if items were decoded properly, use them
+                items = decodedItems
+                return
+            }
+        }
+        
+        // set items to empty if no data.
+        items = []
+    }
 }
